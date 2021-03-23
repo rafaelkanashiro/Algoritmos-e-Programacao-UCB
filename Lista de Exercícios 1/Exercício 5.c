b1f1d69c776a3674ae1f9fe7d2aea32138c3a9cd
Rafael Kanashiro <79936454+rafaelkanashiro@users.noreply.github.com>
2021-03-23 11:58:36 -0300

    Add files via upload

---
+++ Lista de Exercícios 1/Exercício 5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+
+int main(){ 
+	setlocale(LC_ALL, "Portuguese");
+	
+	int quantidadeTotalFios, quantidadeRolos, quantidadeMetros;
+	printf("------------------------------------------------------------------------\n");
+	printf("| Calcular a quantidade de rolos e de metros avulsos de uma construção |\n");
+	printf("------------------------------------------------------------------------\n");
+	printf("> Insira a quantidade total de fios: ");
+	scanf("%d", &quantidadeTotalFios);
+	quantidadeRolos = quantidadeTotalFios / 50;
+	quantidadeMetros = quantidadeTotalFios % 50;
+	if (quantidadeRolos == 0) {
+	printf("> São necessários %d metro(s) avulso(s).\n\n", quantidadeMetros);
+		}
+	else printf("> São necessários %d rolo(s) e %d metro(s) avulso(s).\n\n", quantidadeRolos, quantidadeMetros);
+	
+
+	system("pause");
+ }
